9d5f1d00759959aa9cd1af92ba3a3afaf7085bc4
Tasneemalabsi <tasneem.alabsi@gmail.com>
2023-05-17 11:51:51 +0300

    lab 6

--- test.js
+++ test.js
@@ -1,14 +1,58 @@
 let email = prompt("Please enter your email: ");
-let check = true;
-let arr = email.split("@")
-let str = arr[1];
-let parts = str.split(".");
-while(true){
-    if(email.split("@").length != 2){
-        alert("email is invalid");
+while (true) {
+  let check = true;
+  let count = 0;
+  if (email[0] === "@" || email[0] === " ") {
+    alert("The email is invalid, try again");
+    email = prompt("Please enter your email: ");
+  } else {
+    for (let i = 1; i < email.length; i++) {
+      if (email[i] == " ") {
+        check = false;
+        break;
+      }
+      if (email[i] === "@") {
+        count++;
+      }
     }
-    else if (arr[0].length == 0){
-        alert("email is invalid");
+    if (count === 1 && check) {
+      alert("Valid email");
+      break;
+    } else {
+      alert("The email is invalid, try again");
+      email = prompt("Please enter your email: ");
     }
-    else if (arr[1])
+  }
 }
+
+let password;
+count =0;
+let capital = 0;
+while(true){
+    password=prompt("enter your password");
+    if(password.length<6 || password.length>12){
+        alert("Invalid Password, try again");
+    }
+    else{
+    for(let i=0; i<password.length; i++){
+        if(!isNaN(password[i])){
+            count++;
+        }
+        if(password[i]===password[i].toUpperCase()){
+            capital++
+        }
+    }
+    if(capital>0 && count>0){
+        alert("Valid Password");
+        break;
+    }
+    else{
+        alert("Invalid Password, try again");
+        count = 0;
+        capital=0;
+    }
+    }
+}
+document.write(`<input type="email" value="${email}" style="margin-bottom:20px;">`);
+document.write("<br>");
+document.write(`<input type="password" value="${password}" style="margin-top:20px;"`);
